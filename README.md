c299de2eb6c115c30f0fc7899081b325da67b44e
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-01-24 00:40:40 +0600

    Paywise

---
+++ README.md
@@ -0,0 +1,4 @@
+# [✨ Paywise](https://web.dengrweb.com/view/?id=53) live sell
+
+![cover](p1.png)
+
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+window.onscroll = function () {
+    document.querySelector('#toggle').checked = false;
+}
